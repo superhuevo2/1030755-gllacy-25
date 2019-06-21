98903c56081f7950276ae59de3cdf1486e058683
Anton Appolonov <933375@gmail.com>
2019-06-21 12:40:28 +0300

    исправил разметку главной, джаваскрипт, форму корзины

--- js/script-format.js
+++ js/script-format.js
@@ -6,31 +6,32 @@ var feedbackSend = popupFeedback.querySelector('.button-feedback-send');
 var feedbackInputMail = popupFeedback.querySelector('[name=feedback-email]');
 
 buttonContacts.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    popupFeedback.classList.add('popup-feedback-show');
-    feedbackInputName.focus();
+  evt.preventDefault();
+  popupFeedback.classList.add('popup-feedback-show');
+  feedbackInputName.focus();
 });
 
 popupFeedbackClose.addEventListener('click', function(evt) {
-    popupFeedback.classList.remove('popup-feedback-show');
-    popupFeedback.classList.remove('popup-error');
+  evt.preventDefault();
+  popupFeedback.classList.remove('popup-feedback-show');
+  popupFeedback.classList.remove('popup-error');
 })
 
 window.addEventListener('keydown', function(evt) {
-    if (evt.keyCode === 27) {
-        evt.preventDefault();
-        if (popupFeedback.classList.contains('popup-feedback-show')) {
-            popupFeedback.classList.remove('popup-feedback-show');
-            popupFeedback.classList.remove('popup-error');
-        }
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popupFeedback.classList.contains('popup-feedback-show')) {
+      popupFeedback.classList.remove('popup-feedback-show');
+      popupFeedback.classList.remove('popup-error');
     }
+  }
 })
 
 feedbackSend.addEventListener('click', function(evt) {
-    if (!feedbackInputName.value || !feedbackInputMail.value) {
-        evt.preventDefault();
-        popupFeedback.classList.remove('popup-error');
-        popupFeedback.offsetWidth = popupFeedback.offsetWidth;
-        popupFeedback.classList.add('popup-error');
-    }
+  if (!feedbackInputName.value || !feedbackInputMail.value) {
+    evt.preventDefault();
+    popupFeedback.classList.remove('popup-error');
+    popupFeedback.offsetWidth = popupFeedback.offsetWidth;
+    popupFeedback.classList.add('popup-error');
+  }
 })
